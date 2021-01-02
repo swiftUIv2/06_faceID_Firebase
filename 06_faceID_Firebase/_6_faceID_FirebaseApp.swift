36f8af1fc456fc5956e6f1dbbd68778b56b5badb
emm <723125@gmail.com>
2021-01-02 16:13:37 +0000

    Initial Commit

---
+++ 06_faceID_Firebase/_6_faceID_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_faceID_FirebaseApp.swift
+//  06_faceID_Firebase
+//
+//  Created by emm on 02/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct _6_faceID_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
